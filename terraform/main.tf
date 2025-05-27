76f488bdff17789ba8150f7643a179c69795a845
TonyDfouny <90046057+TonyDfouny@users.noreply.github.com>
2025-05-27 16:02:39 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "ghost_sg" {
 # EC2 instance running Ghost CMS
 resource "aws_instance" "ghost" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.ghost_sg.id]
 
   user_data = <<-EOF
